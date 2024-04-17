54609c24adb6e43540b15dd347d35e443006b3a3
hesptech <olea.oswaldo@gmail.com>
2024-04-17 10:53:35 +0200

    First commit

---
+++ lib/presentation/blocs/01-simple_cubit/username_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+
+
+
+
+class UsernameCubit extends Cubit<String> {
+  
+  UsernameCubit(): super('no-username') {
+    print('UsernameCubit Constructor called');
+  }
+
+  void setUsername(String username) {
+    emit(username);
+  }
+}
+
---
+++ lib/presentation/screens/01_simple_cubit/cubit_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:blocs_app/config/config.dart';
+import 'package:blocs_app/presentation/blocs/blocs.dart';
+
+
+class CubitScreen extends StatelessWidget {
+  const CubitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    //final usernameCubit = context.watch<UsernameCubit>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cubit'),
+      ),
+      body: Center(
+        //child: Text(usernameCubit.state),
+
+        child: BlocBuilder<UsernameCubit, String>(
+          // buildWhen: (previous, current) => previous != current,
+          builder: (context, state) {
+            print('Cambió');
+            return Text(state);
+          },
+        )
+
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<UsernameCubit>().setUsername(
+            RandomGenerator.getRandomName()
+            //'Fernando'
+          );
+          /* usernameCubit.setUsername(
+            RandomGenerator.getRandomName()
+          ); */
+
+        },
+        child: const Icon(Icons.refresh),
+      ),
+    );
+  }
+}
